4d75bebde97c14020626660d001702e76e30cc34
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2023-01-04 02:40:11 +0100

    adapt for Fluvius 3X230V output with ESP32 dev kit

--- README.md
+++ README.md
@@ -1,26 +1,35 @@
 # esphome-p1reader
+
+```diff
+- ADAPTED FOR FLUVIUS BELGIUM, 3 PHASE METER
+```
+
 ESPHome custom component for reading P1 data from electricity meters. Designed for Swedish meters that implements the specification defined in the [Swedish Energy Industry Recommendation For Customer Interfaces](https://www.energiforetagen.se/forlag/elnat/branschrekommendation-for-lokalt-kundgranssnitt-for-elmatare/) version 1.3 and above.
 
 ## ESPHome version
+
 The current version in main is tested with ESPHome version `2022.12.1`. Make sure your ESPHome version is up to date if you experience compile problems.
 
 ## Verified meter hardware / supplier
-* [Sagemcom T211](https://www.ellevio.se/globalassets/uploads/2020/nya-elmatare/ellevio_produktblad_fas3_t211_web2.pdf) / Ellevio & Skånska Energi
-* [Landis+Gyr E360](https://eu.landisgyr.com/blog-se/e360-en-smart-matare-som-optimerarden-totala-agandekostnaden)
-* [Itron A300](https://boraselnat.se/elnat/elmatarbyte-2020-2021/sa-har-fungerar-din-nya-elmatare/) / Borås Elnät
-* [S34U18 (Sanxing SX631)](https://www.vattenfalleldistribution.se/matarbyte/nya-elmataren/) / Vattenfall 
-* [KAIFA MA304H4E](https://reko.nackaenergi.se/elmatarbyte/) / Nacka Energi
-* [KAIFA CL109](https://www.oresundskraft.se/dags-for-matarbyte/) / Öresundskraft
-* [Aidon](https://www.tekniskaverken.se/kundservice/dinamatare/snart-far-du-nya-matare/) / Tekniska Verken
 
-*Note:* There's currently a bug in the E360 firmware, causing it to stop sending out data after a while. Check this comment for more info: https://github.com/psvanstrom/esphome-p1reader/issues/4#issuecomment-810794020
-  
-*Warning:*  Do not confuse KAIFA MA304H4**E** with MA304H4**D** as the latter uses M-Bus instead of P1. Apart from being incompatible protocols, M-Bus pin 1 exerts 27V instead of 5V and will fry your P1 equipment.
+- [Sagemcom T211](https://www.ellevio.se/globalassets/uploads/2020/nya-elmatare/ellevio_produktblad_fas3_t211_web2.pdf) / Ellevio & Skånska Energi
+- [Landis+Gyr E360](https://eu.landisgyr.com/blog-se/e360-en-smart-matare-som-optimerarden-totala-agandekostnaden)
+- [Itron A300](https://boraselnat.se/elnat/elmatarbyte-2020-2021/sa-har-fungerar-din-nya-elmatare/) / Borås Elnät
+- [S34U18 (Sanxing SX631)](https://www.vattenfalleldistribution.se/matarbyte/nya-elmataren/) / Vattenfall
+- [KAIFA MA304H4E](https://reko.nackaenergi.se/elmatarbyte/) / Nacka Energi
+- [KAIFA CL109](https://www.oresundskraft.se/dags-for-matarbyte/) / Öresundskraft
+- [Aidon](https://www.tekniskaverken.se/kundservice/dinamatare/snart-far-du-nya-matare/) / Tekniska Verken
+
+_Note:_ There's currently a bug in the E360 firmware, causing it to stop sending out data after a while. Check this comment for more info: https://github.com/psvanstrom/esphome-p1reader/issues/4#issuecomment-810794020
+
+_Warning:_ Do not confuse KAIFA MA304H4**E** with MA304H4**D** as the latter uses M-Bus instead of P1. Apart from being incompatible protocols, M-Bus pin 1 exerts 27V instead of 5V and will fry your P1 equipment.
 
 ## Hardware
+
 I have used an ESP-12 based NodeMCU for my circuit, another alternative is the cheaper Wemos D1 mini but most ESP-based controllers would probably work. The P1 port on the meter provides 5V up to 250mA which makes it possible to power the circuit directly from the P1 port.
 
 ### Parts
+
 - 1 NodeMCU, Wemos D1 mini or equivalent ESP-12 / ESP-32 microcontroller
 - 1 BC547 / 2N3904 NPN transistor
 - 1 4.7kOhm Resistor
@@ -29,21 +38,27 @@ I have used an ESP-12 based NodeMCU for my circuit, another alternative is the c
 - 1 RJ12 to RJ12 cable (6 wires)
 
 ### Wiring
+
 The circuit is very simple, basically the 5V TX output on the P1 connector is converted to 3.3V and inverted by the transistor and connected to the UART0 RX pin on the microcontroller. The RTS (request to send) pin is pulled high so that data is sent continously and GND and 5V is taken from the P1 connector to drive the microcontroller.
 
 #### Wiring NodeMCU ESP-12
+
 ![Wiring Diagram](images/wiring.png)
 
 #### Wiring Wemos D1 mini
+
 ![image](https://user-images.githubusercontent.com/5547521/132756141-53941ed7-64f6-4c83-b0b0-6fc7c9634752.png)
 
 #### Wiring barebone ESP-12 with added voltage regulators and capacitors.
-The schematics show a 2SC1815 NPN transistor being used (because that's what I had laying around), will work just fine with either one of the transistors listed under the parts section. 
+
+The schematics show a 2SC1815 NPN transistor being used (because that's what I had laying around), will work just fine with either one of the transistors listed under the parts section.
 ![Wiring Diagram](images/p1reader-barebone-ESP-12F.png)
 
 ### PCB and enclosures
+
 #### Naesstrom
-[Naesstrom](https://github.com/Naesstrom) has made a nice PCB layout for the P1 reader using a Wemos D1 mini as the controller along with a 3D printable enclosure. 
+
+[Naesstrom](https://github.com/Naesstrom) has made a nice PCB layout for the P1 reader using a Wemos D1 mini as the controller along with a 3D printable enclosure.
 
 Check out the PCB here: https://oshwlab.com/Naesstrom/esphome-p1reader and the enclosure here: https://www.thingiverse.com/thing:4961372.
 
@@ -53,6 +68,7 @@ Check out the PCB here: https://oshwlab.com/Naesstrom/esphome-p1reader and the e
 </p>
 
 #### EHjortberg
+
 [EHjortberg](https://github.com/ehjortberg) has made an equally nice PCB layout based on an ESP07 module along with a 3D printable enclosure. Check it out here: https://github.com/ehjortberg/kicad-p1-port-thingie.
 
 <p float="left">
@@ -60,6 +76,7 @@ Check out the PCB here: https://oshwlab.com/Naesstrom/esphome-p1reader and the e
 </p>
 
 ## Optional hardware
+
 Weigu has designed [SmartyReader P1](http://weigu.lu/microcontroller/smartyReader_P1/index.html) that also can be running with this code and configuration with a few small adaptions.
 
 This hardware runs on ESP8266 Wemos D1 mini pro but with less components.
@@ -67,11 +84,13 @@ This hardware runs on ESP8266 Wemos D1 mini pro but with less components.
 ### Basic steps to run this code on SmartyReader P1:
 
 1. Set the board to Wemos D1 mini pro
+
 ```
 board: d1_mini_pro
 ```
 
 2. Adjust the UART section to invert the RX pin (removed TX pin config since it is not used).
+
 ```
 uart:
     id: uart_bus
@@ -105,7 +124,9 @@ uart:
 Image credit: https://github.com/Josverl/micropython-p1meter
 
 ## Installation
+
 Clone the repository and create a companion `secrets.yaml` file with the following fields:
+
 ```
 wifi_ssid: <your wifi SSID>
 wifi_password: <your wifi password>
@@ -113,17 +134,18 @@ fallback_password: <fallback AP password>
 hass_api_password: <the Home Assistant API password>
 ota_password: <The OTA password>
 ```
+
 Make sure to place the `secrets.yaml` file in the root path of the cloned project. The `fallback_password` and `ota_password` fields can be set to any password before doing the initial upload of the firmware.
 
-
-
 Prepare the microcontroller with ESPHome before you connect it to the circuit:
+
 - Install the `esphome` [command line tool](https://esphome.io/guides/getting_started_command_line.html)
 - Plug in the microcontroller to your USB port and run `esphome run p1reader.yaml` to flash the firmware
 - Remove the USB connection and connect the microcontroller to the rest of the circuit and plug it into the P1 port.
 - If everything works, your Home Assistant will now auto detect your new ESPHome integration.
 
 You can check the logs by issuing `esphome p1reader.yaml logs` (or use the super awesome ESPHome dashboard available as a Hass.io add-on or standalone). The logs should output data similar to this every 10 seconds when using `DEBUG` loglevel:
+
 ```
 [18:40:01][D][data:264]: /ELL5\253833635_A
 [18:40:01][D][data:264]:
@@ -161,6 +183,7 @@ You can check the logs by issuing `esphome p1reader.yaml logs` (or use the super
 The last row contains the CRC check. If you constantly get invalid CRC there might be something wrong with the serial communication.
 
 ## Technical documentation
+
 Specification overview:
 https://www.tekniskaverken.se/siteassets/tekniska-verken/elnat/aidonfd-rj12-han-interface-se-v13a.cleaned.pdf
 
--- p1reader.h
+++ p1reader.h
@@ -1,20 +1,20 @@
 //-------------------------------------------------------------------------------------
 // ESPHome P1 Electricity Meter custom sensor
 // Copyright 2020 Pär Svanström
-// 
+//
 // History
 //  0.1.0 2020-11-05:   Initial release
 //
 // MIT License
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-------------------------------------------------------------------------------------
 
@@ -22,293 +22,264 @@
 
 #define BUF_SIZE 60
 
-class ParsedMessage {
-  public:
-    double cumulativeActiveImport;
-    double cumulativeActiveExport;
+class ParsedMessage
+{
+public:
+  double eActiveIn;       // derived
+  double eActiveInDay;    // 1.8.1
+  double eActiveInNight;  // 1.8.2
+  double eActiveOut;      // derived
+  double eActiveOutDay;   // 2.8.1
+  double eActiveOutNight; // 2.8.2
+  double pIn;             // 1.7.0
+  double pOut;            // 2.7.0
+  double u1;              // 32.7.0
+  double u2;              // 52.7.0
+  double u3;              // 72.7.0
+  double i1;              // 31.7.0
+  double i2;              // 51.7.0
+  double i3;              // 71.7.0
+  double p1In;            // 21.7.0
+  double p2In;            // 41.7.0
+  double p3In;            // 61.7.0
+  double p1Out;           // 22.7.0
+  double p2Out;           // 42.7.0
+  double p3Out;           // 62.7.0
 
-    double cumulativeReactiveImport;
-    double cumulativeReactiveExport;
-
-    double momentaryActiveImport;
-    double momentaryActiveExport;
-
-    double momentaryReactiveImport;
-    double momentaryReactiveExport;
-
-    double momentaryActiveImportL1;
-    double momentaryActiveExportL1;
-
-    double momentaryActiveImportL2;
-    double momentaryActiveExportL2;
-
-    double momentaryActiveImportL3;
-    double momentaryActiveExportL3;
-
-    double momentaryReactiveImportL1;
-    double momentaryReactiveExportL1;
-
-    double momentaryReactiveImportL2;
-    double momentaryReactiveExportL2;
-
-    double momentaryReactiveImportL3;
-    double momentaryReactiveExportL3;
-
-    double voltageL1;
-    double voltageL2;
-    double voltageL3;
-
-    double currentL1;
-    double currentL2;
-    double currentL3;
-
-    bool crcOk = false;
+  bool crcOk = false;
 };
 
-class P1Reader : public Component, public UARTDevice {
-  const char* DELIMITERS = "()*:";
-  const char* DATA_ID = "1-0";
+class P1Reader : public Component, public UARTDevice
+{
+  const char *DELIMITERS = "()*:";
+  const char *DATA_ID = "1-0";
   char buffer[BUF_SIZE];
 
-  public:
-    Sensor *cumulativeActiveImport = new Sensor();
-    Sensor *cumulativeActiveExport = new Sensor();
+public:
+  Sensor *eActiveIn = new Sensor();
+  Sensor *eActiveInDay = new Sensor();
+  Sensor *eActiveInNight = new Sensor();
+  Sensor *eActiveOut = new Sensor();
+  Sensor *eActiveOutDay = new Sensor();
+  Sensor *eActiveOutNight = new Sensor();
+  Sensor *pIn = new Sensor();
+  Sensor *pOut = new Sensor();
+  Sensor *u1 = new Sensor();
+  Sensor *u2 = new Sensor();
+  Sensor *u3 = new Sensor();
+  Sensor *i1 = new Sensor();
+  Sensor *i2 = new Sensor();
+  Sensor *i3 = new Sensor();
+  Sensor *p1In = new Sensor();
+  Sensor *p2In = new Sensor();
+  Sensor *p3In = new Sensor();
+  Sensor *p1Out = new Sensor();
+  Sensor *p2Out = new Sensor();
+  Sensor *p3Out = new Sensor();
 
-    Sensor *cumulativeReactiveImport = new Sensor();
-    Sensor *cumulativeReactiveExport = new Sensor();
+  P1Reader(UARTComponent *parent) : UARTDevice(parent) {}
 
-    Sensor *momentaryActiveImport = new Sensor();
-    Sensor *momentaryActiveExport = new Sensor();
+  void setup() override {}
 
-    Sensor *momentaryReactiveImport = new Sensor();
-    Sensor *momentaryReactiveExport = new Sensor();
+  void loop() override
+  {
+    readP1Message();
+  }
 
-    Sensor *momentaryActiveImportL1 = new Sensor();
-    Sensor *momentaryActiveExportL1 = new Sensor();
-
-    Sensor *momentaryActiveImportL2 = new Sensor();
-    Sensor *momentaryActiveExportL2 = new Sensor();
-
-    Sensor *momentaryActiveImportL3 = new Sensor();
-    Sensor *momentaryActiveExportL3 = new Sensor();
-
-    Sensor *momentaryReactiveImportL1 = new Sensor();
-    Sensor *momentaryReactiveExportL1 = new Sensor();
-
-    Sensor *momentaryReactiveImportL2 = new Sensor();
-    Sensor *momentaryReactiveExportL2 = new Sensor();
-
-    Sensor *momentaryReactiveImportL3 = new Sensor();
-    Sensor *momentaryReactiveExportL3 = new Sensor();
-
-    Sensor *voltageL1 = new Sensor();
-    Sensor *voltageL2 = new Sensor();
-    Sensor *voltageL3 = new Sensor();
-
-    Sensor *currentL1 = new Sensor();
-    Sensor *currentL2 = new Sensor();
-    Sensor *currentL3 = new Sensor();
-
-    P1Reader(UARTComponent *parent) : UARTDevice(parent) {}
-
-    void setup() override { }
-
-    void loop() override {
-      readP1Message();
-    }
-
-  private:
-    uint16_t crc16_update(uint16_t crc, uint8_t a) {
-      int i;
-      crc ^= a;
-      for (i = 0; i < 8; ++i) {
-        if (crc & 1) {
-            crc = (crc >> 1) ^ 0xA001;
-        } else {
-            crc = (crc >> 1);
-        }
+private:
+  uint16_t crc16_update(uint16_t crc, uint8_t a)
+  {
+    int i;
+    crc ^= a;
+    for (i = 0; i < 8; ++i)
+    {
+      if (crc & 1)
+      {
+        crc = (crc >> 1) ^ 0xA001;
       }
-      return crc;
-    }
-
-    void parseRow(ParsedMessage* parsed, char* obisCode, char* value) {
-      if (strncmp(obisCode, "1.8.0", 5) == 0) {
-        parsed->cumulativeActiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "2.8.0", 5) == 0) {
-        parsed->cumulativeActiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "3.8.0", 5) == 0) {
-        parsed->cumulativeReactiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "4.8.0", 5) == 0) {
-        parsed->cumulativeReactiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "1.7.0", 5) == 0) {
-        parsed->momentaryActiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "2.7.0", 5) == 0) {
-        parsed->momentaryActiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "3.7.0", 5) == 0) {
-        parsed->momentaryReactiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "4.7.0", 5) == 0) {
-        parsed->momentaryReactiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "21.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "22.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "41.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "42.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "61.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "62.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "23.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "24.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "43.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "44.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "63.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "64.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "32.7.0", 6) == 0) {
-        parsed->voltageL1 = atof(value);
-
-      } else if (strncmp(obisCode, "52.7.0", 6) == 0) {
-        parsed->voltageL2 = atof(value);
-
-      } else if (strncmp(obisCode, "72.7.0", 6) == 0) {
-        parsed->voltageL3 = atof(value);
-
-      } else if (strncmp(obisCode, "31.7.0", 6) == 0) {
-        parsed->currentL1 = atof(value);
-
-      } else if (strncmp(obisCode, "51.7.0", 6) == 0) {
-        parsed->currentL2 = atof(value);
-
-      } else if (strncmp(obisCode, "71.7.0", 6) == 0) {
-        parsed->currentL3 = atof(value);
+      else
+      {
+        crc = (crc >> 1);
       }
     }
+    return crc;
+  }
 
-    void publishSensors(ParsedMessage* parsed) {
-      cumulativeActiveImport->publish_state(parsed->cumulativeActiveImport);
-      cumulativeActiveExport->publish_state(parsed->cumulativeActiveExport);
-
-      cumulativeReactiveImport->publish_state(parsed->cumulativeReactiveImport);
-      cumulativeReactiveExport->publish_state(parsed->cumulativeReactiveExport);
-
-      momentaryActiveImport->publish_state(parsed->momentaryActiveImport);
-      momentaryActiveExport->publish_state(parsed->momentaryActiveExport);
-
-      momentaryReactiveImport->publish_state(parsed->momentaryReactiveImport);
-      momentaryReactiveExport->publish_state(parsed->momentaryReactiveExport);
-
-      momentaryActiveImportL1->publish_state(parsed->momentaryActiveImportL1);
-      momentaryActiveExportL1->publish_state(parsed->momentaryActiveExportL1);
-
-      momentaryActiveImportL2->publish_state(parsed->momentaryActiveImportL2);
-      momentaryActiveExportL2->publish_state(parsed->momentaryActiveExportL2);
-
-      momentaryActiveImportL3->publish_state(parsed->momentaryActiveImportL3);
-      momentaryActiveExportL3->publish_state(parsed->momentaryActiveExportL3);
-
-      momentaryReactiveImportL1->publish_state(parsed->momentaryReactiveImportL1);
-      momentaryReactiveExportL1->publish_state(parsed->momentaryReactiveExportL1);
-
-      momentaryReactiveImportL2->publish_state(parsed->momentaryReactiveImportL2);
-      momentaryReactiveExportL2->publish_state(parsed->momentaryReactiveExportL2);
-
-      momentaryReactiveImportL3->publish_state(parsed->momentaryReactiveImportL3);
-      momentaryReactiveExportL3->publish_state(parsed->momentaryReactiveExportL3);
-
-      voltageL1->publish_state(parsed->voltageL1);
-      voltageL2->publish_state(parsed->voltageL2);
-      voltageL3->publish_state(parsed->voltageL3);
-
-      currentL1->publish_state(parsed->currentL1);
-      currentL2->publish_state(parsed->currentL2);
-      currentL3->publish_state(parsed->currentL3);
+  void parseRow(ParsedMessage *parsed, char *obisCode, char *value)
+  {
+    if (strncmp(obisCode, "1.8.1", 5) == 0)
+    {
+      parsed->eActiveInDay = atof(value);
     }
+    else if (strncmp(obisCode, "1.8.2", 5) == 0)
+    {
+      parsed->eActiveInNight = atof(value);
+    }
+    else if (strncmp(obisCode, "2.8.1", 5) == 0)
+    {
+      parsed->eActiveOutDay = atof(value);
+    }
+    else if (strncmp(obisCode, "2.8.2", 5) == 0)
+    {
+      parsed->eActiveOutNight = atof(value);
+    }
+    else if (strncmp(obisCode, "1.7.0", 5) == 0)
+    {
+      parsed->pIn = atof(value);
+    }
+    else if (strncmp(obisCode, "2.7.0", 5) == 0)
+    {
+      parsed->pOut = atof(value);
+    }
+    else if (strncmp(obisCode, "32.7.0", 6) == 0)
+    {
+      parsed->u1 = atof(value);
+    }
+    else if (strncmp(obisCode, "52.7.0", 6) == 0)
+    {
+      parsed->u2 = atof(value);
+    }
+    else if (strncmp(obisCode, "72.7.0", 6) == 0)
+    {
+      parsed->u3 = atof(value);
+    }
+    else if (strncmp(obisCode, "31.7.0", 6) == 0)
+    {
+      parsed->i1 = atof(value);
+    }
+    else if (strncmp(obisCode, "51.7.0", 6) == 0)
+    {
+      parsed->i2 = atof(value);
+    }
+    else if (strncmp(obisCode, "71.7.0", 6) == 0)
+    {
+      parsed->i3 = atof(value);
+    }
+    else if (strncmp(obisCode, "21.7.0", 6) == 0)
+    {
+      parsed->p1In = atof(value);
+    }
+    else if (strncmp(obisCode, "41.7.0", 6) == 0)
+    {
+      parsed->p2In = atof(value);
+    }
+    else if (strncmp(obisCode, "61.7.0", 6) == 0)
+    {
+      parsed->p3In = atof(value);
+    }
+    else if (strncmp(obisCode, "22.7.0", 6) == 0)
+    {
+      parsed->p1Out = atof(value);
+    }
+    else if (strncmp(obisCode, "42.7.0", 6) == 0)
+    {
+      parsed->p2Out = atof(value);
+    }
+    else if (strncmp(obisCode, "62.7.0", 6) == 0)
+    {
+      parsed->p3Out = atof(value);
+    }
+  }
 
-    void readP1Message() {
-      if (available()) {
-        uint16_t crc = 0x0000;
-        ParsedMessage parsed = ParsedMessage();
-        bool telegramEnded = false;
+  void publishSensors(ParsedMessage *parsed)
+  {
+    eActiveIn->publish_state(parsed->eActiveIn);
+    eActiveInDay->publish_state(parsed->eActiveInDay);
+    eActiveInNight->publish_state(parsed->eActiveInNight);
+    eActiveOut->publish_state(parsed->eActiveOut);
+    eActiveOutDay->publish_state(parsed->eActiveOutDay);
+    eActiveOutNight->publish_state(parsed->eActiveOutNight);
+    pIn->publish_state(parsed->pIn);
+    pOut->publish_state(parsed->pOut);
+    u1->publish_state(parsed->u1);
+    u2->publish_state(parsed->u2);
+    u3->publish_state(parsed->u3);
+    i1->publish_state(parsed->i1);
+    i2->publish_state(parsed->i2);
+    i3->publish_state(parsed->i3);
+    p1In->publish_state(parsed->p1In);
+    p2In->publish_state(parsed->p2In);
+    p3In->publish_state(parsed->p3In);
+    p1Out->publish_state(parsed->p1Out);
+    p2Out->publish_state(parsed->p2Out);
+    p3Out->publish_state(parsed->p3Out);
+  }
 
-        while (available()) {
-          int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
+  void readP1Message()
+  {
+    if (available())
+    {
+      uint16_t crc = 0x0000;
+      ParsedMessage parsed = ParsedMessage();
+      bool telegramEnded = false;
 
-          if (len > 0) {
-          	ESP_LOGD("data", "%s", buffer);
+      while (available())
+      {
+        int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
 
-            // put newline back as it is required for CRC calculation
-            buffer[len] = '\n';
-            buffer[len + 1] = '\0';
+        if (len > 0)
+        {
+          ESP_LOGD("data", "%s", buffer);
 
-            // if we've reached the CRC checksum, calculate last CRC and compare
-            if (buffer[0] == '!') {
-              crc = crc16_update(crc, buffer[0]);
-              int crcFromMsg = (int) strtol(&buffer[1], NULL, 16);
-              parsed.crcOk = crc == crcFromMsg;
-              ESP_LOGI("crc", "Telegram read. CRC: %04X = %04X. PASS = %s", crc, crcFromMsg, parsed.crcOk ? "YES": "NO");
-              telegramEnded = true;
+          // put newline back as it is required for CRC calculation
+          buffer[len] = '\n';
+          buffer[len + 1] = '\0';
+
+          // if we've reached the CRC checksum, calculate last CRC and compare
+          if (buffer[0] == '!')
+          {
+            crc = crc16_update(crc, buffer[0]);
+            int crcFromMsg = (int)strtol(&buffer[1], NULL, 16);
+            parsed.crcOk = crc == crcFromMsg;
+            ESP_LOGI("crc", "Telegram read. CRC: %04X = %04X. PASS = %s", crc, crcFromMsg, parsed.crcOk ? "YES" : "NO");
+            telegramEnded = true;
 
             // otherwise pass the row through the CRC calculation
-            } else {
-              for (int i = 0; i < len + 1; i++) {
-                crc = crc16_update(crc, buffer[i]);
-              }
-            }
-
-            // if this is a row containing information
-            if (strchr(buffer, '(') != NULL) {
-              char* dataId = strtok(buffer, DELIMITERS);
-              char* obisCode = strtok(NULL, DELIMITERS);
-
-              // ...and this row is a data row, then parse row
-              if (strncmp(DATA_ID, dataId, strlen(DATA_ID)) == 0) {
-                char* value = strtok(NULL, DELIMITERS);
-                char* unit = strtok(NULL, DELIMITERS);
-                parseRow(&parsed, obisCode, value);
-              }
+          }
+          else
+          {
+            for (int i = 0; i < len + 1; i++)
+            {
+              crc = crc16_update(crc, buffer[i]);
             }
           }
-          // clean buffer
-          memset(buffer, 0, BUF_SIZE - 1);
-        
-          if (!telegramEnded && !available()) {
-          	// wait for more data
-          	delay(40);
+
+          // if this is a row containing information
+          if (strchr(buffer, '(') != NULL)
+          {
+            char *dataId = strtok(buffer, DELIMITERS);
+            char *obisCode = strtok(NULL, DELIMITERS);
+
+            // ...and this row is a data row, then parse row
+            if (strncmp(DATA_ID, dataId, strlen(DATA_ID)) == 0)
+            {
+              char *value = strtok(NULL, DELIMITERS);
+              char *unit = strtok(NULL, DELIMITERS);
+              parseRow(&parsed, obisCode, value);
+            }
           }
         }
+        // clean buffer
+        memset(buffer, 0, BUF_SIZE - 1);
 
-        // if the CRC pass, publish sensor values
-        if (parsed.crcOk) {
-          publishSensors(&parsed);
+        if (!telegramEnded && !available())
+        {
+          // wait for more data
+          delay(40);
         }
       }
+
+      // if the CRC pass, publish sensor values
+      if (parsed.crcOk)
+      {
+        parsed.eActiveIn = parsed.eActiveInDay + parsed.eActiveInNight;
+        parsed.eActiveOut = parsed.eActiveOutDay + parsed.eActiveOutNight;
+        publishSensors(&parsed);
+      }
+      else
+      {
+        ESP_LOGD("data", "CRC Check failed");
+      }
     }
+  }
 };
